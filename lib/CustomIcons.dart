3e148530204d2f043992d27dd8294818b8edb7ab
yanjiepeng <382878591@qq.com>
2019-10-16 16:18:28 +0800

    Initial commit

---
+++ lib/CustomIcons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData menu = IconData(0xe900, fontFamily: "CustomIcons");
+  static const IconData option = IconData(0xe902, fontFamily: "CustomIcons");
+
+  static const IconData menu2 = IconData(0xe84b, fontFamily: 'CustomIcons2');
+  static const IconData option2 = IconData(0xe731, fontFamily: 'CustomIcons2');
+}
